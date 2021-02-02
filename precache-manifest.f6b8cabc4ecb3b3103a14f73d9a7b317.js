f3a4c7d24ee951cf3ab1d506eb9576e9201f2bc7
Taha Khozooie <taha@Tahas-MacBook-Pro.local>
2021-02-02 20:09:10 +0330

    Updates

---
+++ precache-manifest.f6b8cabc4ecb3b3103a14f73d9a7b317.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6148bc4aac3288174ff410d1587d13ec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "56e017c38f4147a9f30e",
+    "url": "./static/css/main.8a3cbe9c.chunk.css"
+  },
+  {
+    "revision": "f933fde7530e8bde67d6",
+    "url": "./static/js/2.f02d4db5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f02d4db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e017c38f4147a9f30e",
+    "url": "./static/js/main.a0f56b86.chunk.js"
+  },
+  {
+    "revision": "27730d19b725ba643428",
+    "url": "./static/js/runtime-main.28ee7e71.js"
+  },
+  {
+    "revision": "d9061d3da8601932e98f79ec8ba1c877",
+    "url": "./static/media/videotest.d9061d3d.mp4"
+  },
+  {
+    "revision": "c3c9bffab5f769333ab25321be2f90b6",
+    "url": "./static/media/videothumbnail.module~KHvXYnVG.c3c9bffa.png"
+  },
+  {
+    "revision": "c3c9bffab5f769333ab25321be2f90b6",
+    "url": "./static/media/videothumbnail~KHvXYnVG.c3c9bffa.png"
+  }
+]);
